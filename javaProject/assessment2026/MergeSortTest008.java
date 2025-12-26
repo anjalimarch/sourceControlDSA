57983bdb20853ca5996909c9ce864ccb42cd132f
anjalimarch <anjalimarch24@gmail.com>
2025-12-26 20:17:38 +0530

    Update MergeSortTest008.java

--- javaProject/assessment2026/MergeSortTest008.java
+++ javaProject/assessment2026/MergeSortTest008.java
@@ -4,12 +4,64 @@ public class MergeSortTest008 {
 
     public static void main (String args [] ){
         int [] a = {44,11,8,7,45,76,45,54};
-        mergeSort(a);
+
+        mergeSort(a,  0, a.length-1);
 
     }
 
-    public static void mergeSort(int [] a){
+    public static void mergeSort(int [] a, int low, int high){
+        if (low<= high ){
 
+            int mid = (low +high)/2;
+            mergeSort(a,  low, mid);
+            mergeSort(a,mid+1, high);
+            merge (a, mid, low, high);
+        }
+
+
+
+
+    }
+
+    public static void merge(int [] a, int mid, int low, int high){
+          int i, j, k ;
+        int [] b =new int [100];
+        i= low;
+        j = mid +1;
+        k = low;
+
+        while (i<=mid && j<=high){
+
+            if(a[i]<a[j]){
+
+                b[k]= a[i];
+                i++;
+                k++;
+
+            }else{
+
+
+                b[k]= a[j];
+                j++;
+                k++;
+            }
+
+
+        }
+
+        while (i<=mid ){
+            b[k]= a[i];
+            k++;
+            i++;
+
+        }
+
+        while (j<=high ){
+            b[k]= a[j];
+            k++;
+            j++;
+
+        }
 
 
 
