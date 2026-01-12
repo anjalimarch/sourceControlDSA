b316b44a451bd1db554a2dd2f5d3d952649c6960
anjalimarch <anjalimarch24@gmail.com>
2026-01-12 20:52:51 +0530

    checkin code

---
+++ javaProject/assessment2026/ImplementationLL014.java
@@ -0,0 +1,97 @@
+package assessment2026;
+
+import java.util.LinkedList;
+
+public class ImplementationLL014 {
+
+
+    public static class Node{
+
+         int data;
+         Node next;
+         Node(int data){
+
+             this.data=data;
+
+
+         }
+
+
+    }
+
+   public static class LinkedList{
+
+      Node head = null;
+      Node tail =null;
+
+      void insertAtEnd(int val){
+
+          Node temp = new Node(val);
+
+          if(head==null){
+
+              head = temp;
+
+
+          }else{
+
+                tail.next= temp;
+
+
+
+
+          }
+
+          tail = temp;
+
+
+      }
+
+      void display(){
+          Node temp = head;
+
+          while(temp!=null){
+
+              System.out.println(temp.data+" ");
+              temp = temp.next;
+
+
+          }
+
+
+      }
+
+      int size(){
+
+          Node temp = head;
+          int count = 0;
+          while(temp!=null){
+              count++;
+              temp = temp.next;
+
+
+          }
+          return count;
+
+      }
+
+
+   }
+    public static void main(String args []){
+
+        LinkedList ll = new LinkedList();
+        ll.insertAtEnd(4);
+        ll.insertAtEnd(5);
+        ll.display();
+
+        int sizeOfLL= ll.size();
+        System.out.println(sizeOfLL);
+
+
+    }
+
+
+
+
+
+}
--- javaProject/assessment2026/LL0013.java
+++ javaProject/assessment2026/LL0013.java
@@ -1,46 +1,93 @@
 package assessment2026;
 
-public class LL0013 {
+public  class LL0013 {
+
+
+   public static void display(Node head){
+
+        Node temp =head;
+        while (temp!=null){
+
+            System.out.println(temp.data);
+            temp= temp.next;
+        }
+
+    }
+
+
+    public static void displayr(Node head){
+
+
+        if (head==null){
+
+            return;
+        }
+
+            System.out.println(head.data);
+            displayr(head.next);
+        }
+
+        public static int length(Node head){
+            int count =0;
+             while (head!=null){
+                 count ++;
+                 head=head.next;
+
+
+             }
+
+
+
+         return count;
+        }
 
 
     public static class Node {
 
         int data;
         Node next;
-        Node (int data ){
 
-             this.data = data;
-             this.next= null;
+        Node(int data) {
+
+            this.data = data;
+            this.next = null;
 
         }
 
-
-
-
-
-
     }
 
-    public void addFirst(String data){
 
-    }
+    public static void main(String args[]) {
 
-    public static void main (String args []){
-
-        LL0013 ll = new LL0013();
+        //
 
         Node a = new Node(5);
         Node b = new Node(6);
-        Node c = new Node (7 );
-        Node d= new Node(8);
+        Node c = new Node(7);
+        Node d = new Node(8);
+        Node e = new Node(9);
         //5->3->9->
 
-        a.next=b;
-        System.out.println(a.next);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+       /* System.out.println(a.next.data);
+        System.out.println(a.next.next);
         System.out.println(b);
         System.out.println(b.next);
         System.out.println(c);
+        System.out.println(c.next);
+        System.out.println(d);
+        System.out.println(d.next);
+        System.out.println(e);
+        System.out.println(e.next);*/
 
-
+        displayr(a);
+        int len=  length(a);
+        System.out.println("length linked list--"+len);
     }
+
 }
+
