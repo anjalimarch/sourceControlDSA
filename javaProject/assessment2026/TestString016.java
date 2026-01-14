38291e28aa622bc995e24b17646f0fbfab176afc
anjalimarch <anjalimarch24@gmail.com>
2026-01-14 14:15:18 +0530

    checking in code

---
+++ javaProject/assessment2026/TestString016.java
@@ -0,0 +1,26 @@
+package assessment2026;
+
+import java.util.Scanner;
+
+public class TestString016 {
+
+    public static void main (String args []){
+
+        Scanner sc =new Scanner(System.in);
+        //String newWord = sc.next();
+
+
+        String str = "abc";
+        str = str +"def";
+        str+='c';
+
+        System.out.println(str);
+
+
+
+    }
+
+
+
+
+}
