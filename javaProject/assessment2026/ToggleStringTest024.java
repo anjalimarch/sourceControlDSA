e5004339552eb032481e9f457335f86cbfe539e9
anjalimarch <anjalimarch24@gmail.com>
2026-01-14 20:11:40 +0530

    checking in code

---
+++ javaProject/assessment2026/ToggleStringTest024.java
@@ -0,0 +1,53 @@
+package assessment2026;
+
+public class ToggleStringTest024 {
+
+    // upper to lower and lower to upper
+
+    public static void main (String args []){
+
+
+        String str = "PhYSiCS";
+
+       // StringBuilder sb = new StringBuilder(str);
+      for(int i=0; i<str.length();i++){
+
+
+          boolean flag = true;
+          char ch =str.charAt(i); //A
+
+          if(ch==' '){
+
+              continue;
+
+          }
+
+          int asci = (int)ch; //65
+          if(asci>=97){ //small
+
+              flag=false;
+
+          }
+          if(flag==true){ //capital
+
+              asci+=32;
+              char dh = (char)asci; //a
+              str=str.substring(0,i)+dh+str.substring(i+1);
+
+
+
+          }else { //small
+
+              asci -=32;
+              char dh = (char)asci;
+              str=str.substring(0,i)+dh+str.substring(i+1);
+
+
+          }
+
+      }
+
+
+    }
+
+}
