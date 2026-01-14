4683b61cfeb2d5f54d2424e914630d0a503da312
anjalimarch <anjalimarch24@gmail.com>
2026-01-14 13:59:30 +0530

    checking in code

--- javaProject/assessment2026/TestStrings015.java
+++ javaProject/assessment2026/TestStrings015.java
@@ -8,9 +8,29 @@ public class TestStrings015 {
 
     public static void main (String args []){
 
-        Scanner sc = new Scanner(System.in);
-         String str=  sc.next();
-         System.out.println(str);
+         //Scanner sc = new Scanner(System.in);
+      //   String str=  sc.next();
+        // String strNew= sc.nextLine();
+        // System.out.println(str);
+      //   System.out.println(strNew);
+
+        String str = "Mishra Anjali";
+        int len = str.length();
+        System.out.println(len);
+        char ch = str.charAt(3);
+        System.out.println(ch);
+        int idx = str.indexOf('i');
+        System.out.println(idx);
+        String atr = "Hello";
+        String gtr= "Dello";
+
+        System.out.println(atr.compareTo(gtr));
+        String name= "Anjali Mishra";
+        System.out.println(name.contains("Mis"));
+
+
+
+
 
     }
 
