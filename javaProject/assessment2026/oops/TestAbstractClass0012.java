87883803a07cd90e9937c4b523a72508885e76f5
anjalimarch <anjalimarch24@gmail.com>
2026-01-16 21:44:51 +0530

    checking in code

--- javaProject/assessment2026/oops/TestAbstractClass0012.java
+++ javaProject/assessment2026/oops/TestAbstractClass0012.java
@@ -1,7 +1,50 @@
 package assessment2026.oops;
 
+abstract class Parent2{
+
+    public  Parent2(){
 
 
+        System.out.println("Mai base2 ka constructor hun");
+    }
+
+    public void sayHello(){
+
+        System.out.println("Hello");
+    }
+    abstract public void greet();
+    abstract public void greet2();
+
+}
+
+class Child2 extends Parent2{
+
+  @Override
+  public void greet(){
+      System.out.println("Good Morning");
+
+  }
+
+  @Override
+  public void greet2(){
+
+      System.out.println("Good afternoon");
+
+  }
+
+}
+
+abstract class Child3 extends Parent2{
+
+    public void th(){
+
+
+        System.out.println("I am good");
+    }
+
+
+
+}
 
 
 
@@ -13,6 +56,9 @@ package assessment2026.oops;
 public class TestAbstractClass0012 {
 
     public static void main(String[] args) {
+
+
+
 
     }
 
